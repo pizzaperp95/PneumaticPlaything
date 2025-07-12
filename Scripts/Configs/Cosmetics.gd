01caec7e555c7f20be566b4e966e0ed08fb71fb9
Persephone Bubblegum-Holiday <kawaiizenbo@outlook.com>
2025-07-12 12:58:24 -0700

    many many things
    fixed cyber turning
    added legs for pasqually and jasper
    optimized stage loading code
    fixed ui bugs

---
+++ Scripts/Configs/Cosmetics.gd
@@ -0,0 +1,660 @@
+extends Node
+
+var CyberChuck = {
+	"Chuck E. Hat":
+	{
+		"Derby":
+		{
+			"Chuck/Chuck/Skeleton3D/Avenger Hat": false,
+			"Chuck/Chuck/Skeleton3D/Cool Chuck Hat": false,
+			"Chuck/Chuck/Skeleton3D/Derby": true,
+			"Chuck/Chuck/Skeleton3D/Tux Derby": false,
+			"Chuck/Chuck/Skeleton3D/Blue Cool Chuck Hat": false,
+		},
+		"Tux Derby":
+		{
+			"Chuck/Chuck/Skeleton3D/Avenger Hat": false,
+			"Chuck/Chuck/Skeleton3D/Cool Chuck Hat": false,
+			"Chuck/Chuck/Skeleton3D/Derby": false,
+			"Chuck/Chuck/Skeleton3D/Tux Derby": true,
+			"Chuck/Chuck/Skeleton3D/Blue Cool Chuck Hat": false,
+		},
+		"Cool Chuck":
+		{
+			"Chuck/Chuck/Skeleton3D/Avenger Hat": false,
+			"Chuck/Chuck/Skeleton3D/Cool Chuck Hat": true,
+			"Chuck/Chuck/Skeleton3D/Derby": false,
+			"Chuck/Chuck/Skeleton3D/Tux Derby": false,
+			"Chuck/Chuck/Skeleton3D/Blue Cool Chuck Hat": false,
+		},
+		"Cool Chuck (Blue)":
+		{
+			"Chuck/Chuck/Skeleton3D/Avenger Hat": false,
+			"Chuck/Chuck/Skeleton3D/Cool Chuck Hat": false,
+			"Chuck/Chuck/Skeleton3D/Derby": false,
+			"Chuck/Chuck/Skeleton3D/Tux Derby": false,
+			"Chuck/Chuck/Skeleton3D/Blue Cool Chuck Hat": true,
+		},
+		"Avenger":
+		{
+			"Chuck/Chuck/Skeleton3D/Avenger Hat": true,
+			"Chuck/Chuck/Skeleton3D/Cool Chuck Hat": false,
+			"Chuck/Chuck/Skeleton3D/Derby": false,
+			"Chuck/Chuck/Skeleton3D/Tux Derby": false,
+			"Chuck/Chuck/Skeleton3D/Blue Cool Chuck Hat": false,
+		},
+		"None":
+		{
+			"Chuck/Chuck/Skeleton3D/Avenger Hat": false,
+			"Chuck/Chuck/Skeleton3D/Cool Chuck Hat": false,
+			"Chuck/Chuck/Skeleton3D/Derby": false,
+			"Chuck/Chuck/Skeleton3D/Tux Derby": false,
+			"Chuck/Chuck/Skeleton3D/Blue Cool Chuck Hat": false,
+		}
+	},
+	
+	"Chuck E. Shirt":
+	{
+		"Yellow Checker Vest":
+		{
+			"Chuck/Chuck/Skeleton3D/Rockstar Shirt": false,
+			"Chuck/Chuck/Skeleton3D/Avenger Shirt": false,
+			"Chuck/Chuck/Skeleton3D/Cool Chuck Shirt": false,
+			"Chuck/Chuck/Skeleton3D/Black Bowtie": true,
+			"Chuck/Chuck/Skeleton3D/Buttons": true,
+			"Chuck/Chuck/Skeleton3D/Vest Trim": true,
+			"Chuck/Chuck/Skeleton3D/Yellow Checker Vest": true,
+			"Chuck/Chuck/Skeleton3D/Rocker Vest": false,
+			"Chuck/Chuck/Skeleton3D/Red Bowtie": false,
+			"Chuck/Chuck/Skeleton3D/Tux Button": false,
+			"Chuck/Chuck/Skeleton3D/Lapel": false,
+			"Chuck/Chuck/Skeleton3D/Tux Base": false,
+		},
+		"Red Vest":
+		{
+			"Chuck/Chuck/Skeleton3D/Rockstar Shirt": false,
+			"Chuck/Chuck/Skeleton3D/Avenger Shirt": false,
+			"Chuck/Chuck/Skeleton3D/Cool Chuck Shirt": false,
+			"Chuck/Chuck/Skeleton3D/Black Bowtie": true,
+			"Chuck/Chuck/Skeleton3D/Buttons": true,
+			"Chuck/Chuck/Skeleton3D/Vest Trim": true,
+			"Chuck/Chuck/Skeleton3D/Yellow Checker Vest": false,
+			"Chuck/Chuck/Skeleton3D/Rocker Vest": true,
+			"Chuck/Chuck/Skeleton3D/Red Bowtie": false,
+			"Chuck/Chuck/Skeleton3D/Tux Button": false,
+			"Chuck/Chuck/Skeleton3D/Lapel": false,
+			"Chuck/Chuck/Skeleton3D/Tux Base": false,
+		},
+		"Tux":
+		{
+			"Chuck/Chuck/Skeleton3D/Rockstar Shirt": false,
+			"Chuck/Chuck/Skeleton3D/Avenger Shirt": false,
+			"Chuck/Chuck/Skeleton3D/Cool Chuck Shirt": false,
+			"Chuck/Chuck/Skeleton3D/Black Bowtie": false,
+			"Chuck/Chuck/Skeleton3D/Buttons": false,
+			"Chuck/Chuck/Skeleton3D/Vest Trim": false,
+			"Chuck/Chuck/Skeleton3D/Yellow Checker Vest": false,
+			"Chuck/Chuck/Skeleton3D/Rocker Vest": false,
+			"Chuck/Chuck/Skeleton3D/Red Bowtie": true,
+			"Chuck/Chuck/Skeleton3D/Tux Button": true,
+			"Chuck/Chuck/Skeleton3D/Lapel": true,
+			"Chuck/Chuck/Skeleton3D/Tux Base": true,
+		},
+		"Cool Chuck Shirt":
+		{
+			"Chuck/Chuck/Skeleton3D/Rockstar Shirt": false,
+			"Chuck/Chuck/Skeleton3D/Avenger Shirt": false,
+			"Chuck/Chuck/Skeleton3D/Cool Chuck Shirt": true,
+			"Chuck/Chuck/Skeleton3D/Black Bowtie": false,
+			"Chuck/Chuck/Skeleton3D/Buttons": false,
+			"Chuck/Chuck/Skeleton3D/Vest Trim": false,
+			"Chuck/Chuck/Skeleton3D/Yellow Checker Vest": false,
+			"Chuck/Chuck/Skeleton3D/Rocker Vest": false,
+			"Chuck/Chuck/Skeleton3D/Red Bowtie": false,
+			"Chuck/Chuck/Skeleton3D/Tux Button": false,
+			"Chuck/Chuck/Skeleton3D/Lapel": false,
+			"Chuck/Chuck/Skeleton3D/Tux Base": false,
+		},
+		"Avenger Shirt":
+		{
+			"Chuck/Chuck/Skeleton3D/Rockstar Shirt": false,
+			"Chuck/Chuck/Skeleton3D/Avenger Shirt": true,
+			"Chuck/Chuck/Skeleton3D/Cool Chuck Shirt": false,
+			"Chuck/Chuck/Skeleton3D/Black Bowtie": false,
+			"Chuck/Chuck/Skeleton3D/Buttons": false,
+			"Chuck/Chuck/Skeleton3D/Vest Trim": false,
+			"Chuck/Chuck/Skeleton3D/Yellow Checker Vest": false,
+			"Chuck/Chuck/Skeleton3D/Rocker Vest": false,
+			"Chuck/Chuck/Skeleton3D/Red Bowtie": false,
+			"Chuck/Chuck/Skeleton3D/Tux Button": false,
+			"Chuck/Chuck/Skeleton3D/Lapel": false,
+			"Chuck/Chuck/Skeleton3D/Tux Base": false,
+		},
+		"Rockstar Shirt":
+		{
+			"Chuck/Chuck/Skeleton3D/Rockstar Shirt": true,
+			"Chuck/Chuck/Skeleton3D/Avenger Shirt": false,
+			"Chuck/Chuck/Skeleton3D/Cool Chuck Shirt": false,
+			"Chuck/Chuck/Skeleton3D/Black Bowtie": false,
+			"Chuck/Chuck/Skeleton3D/Buttons": false,
+			"Chuck/Chuck/Skeleton3D/Vest Trim": false,
+			"Chuck/Chuck/Skeleton3D/Yellow Checker Vest": false,
+			"Chuck/Chuck/Skeleton3D/Rocker Vest": false,
+			"Chuck/Chuck/Skeleton3D/Red Bowtie": false,
+			"Chuck/Chuck/Skeleton3D/Tux Button": false,
+			"Chuck/Chuck/Skeleton3D/Lapel": false,
+			"Chuck/Chuck/Skeleton3D/Tux Base": false,
+		}
+	},
+	
+	"Chuck E. Mask":
+	{
+		"PTT":
+		{
+			"Chuck/Chuck/Skeleton3D/PTT Ears": true,
+			"Chuck/Chuck/Skeleton3D/PTT Jaw": true,
+			"Chuck/Chuck/Skeleton3D/PTT Muzzle": true,
+			"Chuck/Chuck/Skeleton3D/Modern Ears": false,
+			"Chuck/Chuck/Skeleton3D/Modern Jaw": false,
+			"Chuck/Chuck/Skeleton3D/Modern Muzzle": false,
+			"Chuck/Chuck/Skeleton3D/Northridge Ears": false,
+		},
+		"Modern":
+		{
+			"Chuck/Chuck/Skeleton3D/PTT Ears": false,
+			"Chuck/Chuck/Skeleton3D/PTT Jaw": false,
+			"Chuck/Chuck/Skeleton3D/PTT Muzzle": false,
+			"Chuck/Chuck/Skeleton3D/Modern Ears": true,
+			"Chuck/Chuck/Skeleton3D/Modern Jaw": true,
+			"Chuck/Chuck/Skeleton3D/Modern Muzzle": true,
+			"Chuck/Chuck/Skeleton3D/Northridge Ears": false,
+		},
+		"Northridge":
+		{
+			"Chuck/Chuck/Skeleton3D/PTT Ears": false,
+			"Chuck/Chuck/Skeleton3D/PTT Jaw": false,
+			"Chuck/Chuck/Skeleton3D/PTT Muzzle": false,
+			"Chuck/Chuck/Skeleton3D/Modern Ears": false,
+			"Chuck/Chuck/Skeleton3D/Modern Jaw": true,
+			"Chuck/Chuck/Skeleton3D/Modern Muzzle": true,
+			"Chuck/Chuck/Skeleton3D/Northridge Ears": true,
+		},
+	},
+	
+	"Chuck E. Eyelids":
+	{
+		"Blue":
+		{
+			"Chuck/Chuck/Skeleton3D/Blue Eyelids": true,
+			"Chuck/Chuck/Skeleton3D/Grey Eyelids": false,
+		},
+		"Grey":
+		{
+			"Chuck/Chuck/Skeleton3D/Blue Eyelids": false,
+			"Chuck/Chuck/Skeleton3D/Grey Eyelids": true,
+		},
+	},
+	
+	"Chuck E. Legs":
+	{
+		"None":
+		{
+			"Chuck/Chuck/Skeleton3D/Stand": true,
+			"Chuck/Chuck/Skeleton3D/Legs": false,
+			"Chuck/Chuck/Skeleton3D/Feet": false,
+		},
+		"Normal":
+		{
+			"Chuck/Chuck/Skeleton3D/Stand": false,
+			"Chuck/Chuck/Skeleton3D/Legs": true,
+			"Chuck/Chuck/Skeleton3D/Feet": true,
+		}
+	},
+	
+	"Chuck E. Pants":
+	{
+		"None":
+		{
+			"Chuck/Chuck/Skeleton3D/Avenger Shorts": false,
+			"Chuck/Chuck/Skeleton3D/Cool Chuck Shorts": false,
+			"Chuck/Chuck/Skeleton3D/Rockstar Jeans": false,
+		},
+		"Cool Chuck Shorts":
+		{
+			"Chuck/Chuck/Skeleton3D/Avenger Shorts": false,
+			"Chuck/Chuck/Skeleton3D/Cool Chuck Shorts": true,
+			"Chuck/Chuck/Skeleton3D/Rockstar Jeans": false,
+		},
+		"Avenger Shorts":
+		{
+			"Chuck/Chuck/Skeleton3D/Avenger Shorts": true,
+			"Chuck/Chuck/Skeleton3D/Cool Chuck Shorts": false,
+			"Chuck/Chuck/Skeleton3D/Rockstar Jeans": false,
+		},
+		"Rockstar Jeans":
+		{
+			"Chuck/Chuck/Skeleton3D/Avenger Shorts": false,
+			"Chuck/Chuck/Skeleton3D/Cool Chuck Shorts": false,
+			"Chuck/Chuck/Skeleton3D/Rockstar Jeans": true,
+		}
+	},
+}
+
+var CyberHelen = {
+	"Helen Eyelids":
+	{
+		"Blue":
+		{
+			"Helen/Helen/Skeleton3D/Blue Eyelids": true,
+			"Helen/Helen/Skeleton3D/Pink Eyelids": false,
+		},
+		"Pink":
+		{
+			"Helen/Helen/Skeleton3D/Blue Eyelids": false,
+			"Helen/Helen/Skeleton3D/Pink Eyelids": true,
+		},
+	},
+	
+	"Helen Dress":
+	{
+		"PTT":
+		{
+			"Helen/Helen/Skeleton3D/PTT Shirt": true,
+			"Helen/Helen/Skeleton3D/Half Body Skirt": false,
+			"Helen/Helen/Skeleton3D/Skirt": false,
+			"Helen/Helen/Skeleton3D/Black Shirt": false,
+			"Helen/Helen/Skeleton3D/Collar": false,
+		},
+		"Road Stage":
+		{
+			"Helen/Helen/Skeleton3D/PTT Shirt": false,
+			"Helen/Helen/Skeleton3D/Half Body Skirt": true,
+			"Helen/Helen/Skeleton3D/Skirt": false,
+			"Helen/Helen/Skeleton3D/Black Shirt": true,
+			"Helen/Helen/Skeleton3D/Collar": true,
+		},
+		"Modern":
+		{
+			"Helen/Helen/Skeleton3D/PTT Shirt": false,
+			"Helen/Helen/Skeleton3D/Half Body Skirt": false,
+			"Helen/Helen/Skeleton3D/Skirt": true,
+			"Helen/Helen/Skeleton3D/Black Shirt": true,
+			"Helen/Helen/Skeleton3D/Collar": true,
+		},
+	},
+	
+	"Helen Hair":
+	{
+		"PTT":
+		{
+			"Helen/Helen/Skeleton3D/Modern Hair": false,
+			"Helen/Helen/Skeleton3D/Modern Hair Yellow": false,
+			"Helen/Helen/Skeleton3D/Road Stage Hair": false,
+			"Helen/Helen/Skeleton3D/PTT Hair": true,
+		},
+		"Road Stage":
+		{
+			"Helen/Helen/Skeleton3D/Modern Hair": false,
+			"Helen/Helen/Skeleton3D/Modern Hair Yellow": false,
+			"Helen/Helen/Skeleton3D/Road Stage Hair": true,
+			"Helen/Helen/Skeleton3D/PTT Hair": false,
+		},
+		"Modern":
+		{
+			"Helen/Helen/Skeleton3D/Modern Hair": true,
+			"Helen/Helen/Skeleton3D/Modern Hair Yellow": false,
+			"Helen/Helen/Skeleton3D/Road Stage Hair": false,
+			"Helen/Helen/Skeleton3D/PTT Hair": false,
+		},
+		"Modern Yellow":
+		{
+			"Helen/Helen/Skeleton3D/Modern Hair": false,
+			"Helen/Helen/Skeleton3D/Modern Hair Yellow": true,
+			"Helen/Helen/Skeleton3D/Road Stage Hair": false,
+			"Helen/Helen/Skeleton3D/PTT Hair": false,
+		},
+	},
+	
+	"Helen Mask":
+	{
+		"PTT":
+		{
+			"Helen/Helen/Skeleton3D/PTT Mask": true,
+			"Helen/Helen/Skeleton3D/PTT Jaw": true,
+			"Helen/Helen/Skeleton3D/PTT Beak": true,
+			"Helen/Helen/Skeleton3D/PTT Mask Crest": true,
+			"Helen/Helen/Skeleton3D/PTT Red Mouth Parts": true,
+			"Helen/Helen/Skeleton3D/PTT Eyeshadow": true,
+			"Helen/Helen/Skeleton3D/Modern Head": false,
+			"Helen/Helen/Skeleton3D/Modern Mask": false,
+			"Helen/Helen/Skeleton3D/Road Stage Jaw": false,
+			"Helen/Helen/Skeleton3D/Road Stage Beak": false,
+			"Helen/Helen/Skeleton3D/Modern Jaw": false,
+			"Helen/Helen/Skeleton3D/Modern Beak": false,
+		},
+		"Road Stage":
+		{
+			"Helen/Helen/Skeleton3D/PTT Mask": false,
+			"Helen/Helen/Skeleton3D/PTT Jaw": false,
+			"Helen/Helen/Skeleton3D/PTT Beak": false,
+			"Helen/Helen/Skeleton3D/PTT Mask Crest": false,
+			"Helen/Helen/Skeleton3D/PTT Red Mouth Parts": false,
+			"Helen/Helen/Skeleton3D/PTT Eyeshadow": false,
+			"Helen/Helen/Skeleton3D/Modern Head": true,
+			"Helen/Helen/Skeleton3D/Modern Mask": true,
+			"Helen/Helen/Skeleton3D/Road Stage Jaw": true,
+			"Helen/Helen/Skeleton3D/Road Stage Beak": true,
+			"Helen/Helen/Skeleton3D/Modern Jaw": false,
+			"Helen/Helen/Skeleton3D/Modern Beak": false,
+		},
+		"Modern":
+		{
+			"Helen/Helen/Skeleton3D/PTT Mask": false,
+			"Helen/Helen/Skeleton3D/PTT Jaw": false,
+			"Helen/Helen/Skeleton3D/PTT Beak": false,
+			"Helen/Helen/Skeleton3D/PTT Mask Crest": false,
+			"Helen/Helen/Skeleton3D/PTT Red Mouth Parts": false,
+			"Helen/Helen/Skeleton3D/PTT Eyeshadow": false,
+			"Helen/Helen/Skeleton3D/Modern Head": true,
+			"Helen/Helen/Skeleton3D/Modern Mask": true,
+			"Helen/Helen/Skeleton3D/Road Stage Jaw": false,
+			"Helen/Helen/Skeleton3D/Road Stage Beak": false,
+			"Helen/Helen/Skeleton3D/Modern Jaw": true,
+			"Helen/Helen/Skeleton3D/Modern Beak": true,
+		},
+	},
+	
+	"Helen Legs":
+	{
+		"None":
+		{
+			"Helen/Helen/Skeleton3D/Stand": true,
+			"Helen/Helen/Skeleton3D/Legs": false,
+			"Helen/Helen/Skeleton3D/Shoes": false,
+		},
+		"Modern Standing":
+		{
+			"Helen/Helen/Skeleton3D/Stand": false,
+			"Helen/Helen/Skeleton3D/Legs": true,
+			"Helen/Helen/Skeleton3D/Shoes": true,
+		}
+	},
+}
+
+var CyberMunch = {
+	"Munch Hat":
+	{
+		"None":
+		{
+			"Munch/Munch/Skeleton3D/Road Stage Hat": false,
+			"Munch/Munch/Skeleton3D/Modern Hat": false,
+		},
+		"Road Stage":
+		{
+			"Munch/Munch/Skeleton3D/Road Stage Hat": true,
+			"Munch/Munch/Skeleton3D/Modern Hat": false,
+		},
+		"Modern":
+		{
+			"Munch/Munch/Skeleton3D/Road Stage Hat": false,
+			"Munch/Munch/Skeleton3D/Modern Hat": true,
+		},
+	},
+	
+	"Munch Mask":
+	{
+		"PTT":
+		{
+			"Munch/Munch/Skeleton3D/PTT Nostrils": true,
+			"Munch/Munch/Skeleton3D/Modern Nostrils": false,
+		},
+		"Modern":
+		{
+			"Munch/Munch/Skeleton3D/PTT Nostrils": false,
+			"Munch/Munch/Skeleton3D/Modern Nostrils": true,
+		},
+	},
+	
+	"Munch Body":
+	{
+		"PTT":
+		{
+			"Munch/Munch/Skeleton3D/PTT Torso": true,
+			"Munch/Munch/Skeleton3D/PTT Arms": true,
+			"Munch/Munch/Skeleton3D/PTT Left Hand": true,
+			"Munch/Munch/Skeleton3D/PTT Right Hand": true,
+			"Munch/Munch/Skeleton3D/Modern Torso": false,
+			"Munch/Munch/Skeleton3D/Modern Arms": false,
+			"Munch/Munch/Skeleton3D/Modern Left Hand": false,
+			"Munch/Munch/Skeleton3D/Modern Right Hand": false,
+		},
+		"Modern":
+		{
+			"Munch/Munch/Skeleton3D/PTT Torso": false,
+			"Munch/Munch/Skeleton3D/PTT Arms": false,
+			"Munch/Munch/Skeleton3D/PTT Left Hand": false,
+			"Munch/Munch/Skeleton3D/PTT Right Hand": false,
+			"Munch/Munch/Skeleton3D/Modern Torso": true,
+			"Munch/Munch/Skeleton3D/Modern Arms": true,
+			"Munch/Munch/Skeleton3D/Modern Left Hand": true,
+			"Munch/Munch/Skeleton3D/Modern Right Hand": true,
+		},
+	},
+	
+	"Munch Legs":
+	{
+		"None":
+		{
+			"Munch/Munch/Skeleton3D/Legs": false,
+			"Munch/Munch/Skeleton3D/Shoes": false,
+		},
+		"Modern":
+		{
+			"Munch/Munch/Skeleton3D/Legs": true,
+			"Munch/Munch/Skeleton3D/Shoes": true,
+		},
+	},
+}
+
+var CyberJasper = {
+	"Jasper Hat":
+	{
+		"PTT":
+		{
+			"Jasper/Jasper/Skeleton3D/Straw Hat Small": true,
+			"Jasper/Jasper/Skeleton3D/Straw Hat Large": false,
+			"Jasper/Jasper/Skeleton3D/Cowboy Hat": false,
+		},
+		"Rocker":
+		{
+			"Jasper/Jasper/Skeleton3D/Straw Hat Small": false,
+			"Jasper/Jasper/Skeleton3D/Straw Hat Large": true,
+			"Jasper/Jasper/Skeleton3D/Cowboy Hat": false,
+		},
+		"Modern":
+		{
+			"Jasper/Jasper/Skeleton3D/Straw Hat Small": false,
+			"Jasper/Jasper/Skeleton3D/Straw Hat Large": false,
+			"Jasper/Jasper/Skeleton3D/Cowboy Hat": true,
+		},
+	},
+	
+	"Jasper Shirt":
+	{
+		"PTT":
+		{
+			"Jasper/Jasper/Skeleton3D/Red Shirt": true,
+			"Jasper/Jasper/Skeleton3D/Rocker Shirt": false,
+			"Jasper/Jasper/Skeleton3D/Cow Print Shirt": false,
+		},
+		"Rocker":
+		{
+			"Jasper/Jasper/Skeleton3D/Red Shirt": false,
+			"Jasper/Jasper/Skeleton3D/Rocker Shirt": true,
+			"Jasper/Jasper/Skeleton3D/Cow Print Shirt": false,
+		},
+		"Modern":
+		{
+			"Jasper/Jasper/Skeleton3D/Red Shirt": false,
+			"Jasper/Jasper/Skeleton3D/Rocker Shirt": false,
+			"Jasper/Jasper/Skeleton3D/Cow Print Shirt": true,
+		},
+	},
+	
+	"Jasper Guitar":
+	{
+		"Banjo":
+		{
+			"Jasper/Jasper/Skeleton3D/Banjo": true,
+			"Jasper/Jasper/Skeleton3D/Cheese Guitar": false,
+		},
+		"Cheese Guitar":
+		{
+			"Jasper/Jasper/Skeleton3D/Banjo": false,
+			"Jasper/Jasper/Skeleton3D/Cheese Guitar": true,
+		},
+	},
+	
+	"Jasper Bandana":
+	{
+		"Blue":
+		{
+			"Jasper/Jasper/Skeleton3D/Blue Bandana": true,
+			"Jasper/Jasper/Skeleton3D/Red Bandana": false,
+		},
+		"Red":
+		{
+			"Jasper/Jasper/Skeleton3D/Blue Bandana": false,
+			"Jasper/Jasper/Skeleton3D/Red Bandana": true,
+		},
+	},
+	
+	"Jasper Overalls":
+	{
+		"Normal":
+		{
+			"Jasper/Jasper/Skeleton3D/Overalls": true,
+		},
+		"None":
+		{
+			"Jasper/Jasper/Skeleton3D/Overalls": false,
+		},
+	},
+	
+	"Jasper Legs":
+	{
+		"None":
+		{
+			"Jasper/Jasper/Skeleton3D/Pants": false,
+			"Jasper/Jasper/Skeleton3D/Shoes": false,
+		},
+		"Normal":
+		{
+			"Jasper/Jasper/Skeleton3D/Pants": true,
+			"Jasper/Jasper/Skeleton3D/Shoes": true,
+		},
+	},
+}
+
+var CyberPasqually = {
+	"Pasqually Body":
+	{
+		"PTT":
+		{
+			"Pasqually/Pasqually/Skeleton3D/PTT Torso": true,
+			"Pasqually/Pasqually/Skeleton3D/PTT Arms": true,
+			"Pasqually/Pasqually/Skeleton3D/PTT Left Hand": true,
+			"Pasqually/Pasqually/Skeleton3D/PTT Right Hand": true,
+			"Pasqually/Pasqually/Skeleton3D/Modern Torso": false,
+			"Pasqually/Pasqually/Skeleton3D/Modern Arms": false,
+			"Pasqually/Pasqually/Skeleton3D/Modern Left Hand": false,
+			"Pasqually/Pasqually/Skeleton3D/Modern Right Hand": false,
+		},
+		"Modern":
+		{
+			"Pasqually/Pasqually/Skeleton3D/PTT Torso": false,
+			"Pasqually/Pasqually/Skeleton3D/PTT Arms": false,
+			"Pasqually/Pasqually/Skeleton3D/PTT Left Hand": false,
+			"Pasqually/Pasqually/Skeleton3D/PTT Right Hand": false,
+			"Pasqually/Pasqually/Skeleton3D/Modern Torso": true,
+			"Pasqually/Pasqually/Skeleton3D/Modern Arms": true,
+			"Pasqually/Pasqually/Skeleton3D/Modern Left Hand": true,
+			"Pasqually/Pasqually/Skeleton3D/Modern Right Hand": true,
+		},
+	},
+	
+	"Pasqually Legs":
+	{
+		"None":
+		{
+			"Pasqually/Pasqually/Skeleton3D/Legs": false,
+			"Pasqually/Pasqually/Skeleton3D/Shoes": false,
+		},
+		"Normal":
+		{
+			"Pasqually/Pasqually/Skeleton3D/Legs": true,
+			"Pasqually/Pasqually/Skeleton3D/Shoes": true,
+		},
+	},
+	
+	"Pasqually Held Item":
+	{
+		"Accordion":
+		{
+			"Pasqually/Pasqually/Skeleton3D/Spoons": false,
+			"Pasqually/Pasqually/Skeleton3D/Drumsticks": false,
+			"Pasqually/Pasqually/Skeleton3D/Accordion": true,
+		},
+		"None":
+		{
+			"Pasqually/Pasqually/Skeleton3D/Spoons": false,
+			"Pasqually/Pasqually/Skeleton3D/Drumsticks": false,
+			"Pasqually/Pasqually/Skeleton3D/Accordion": false,
+		},
+		"Spoons":
+		{
+			"Pasqually/Pasqually/Skeleton3D/Spoons": true,
+			"Pasqually/Pasqually/Skeleton3D/Drumsticks": false,
+			"Pasqually/Pasqually/Skeleton3D/Accordion": false,
+		},
+		"Drumsticks":
+		{
+			"Pasqually/Pasqually/Skeleton3D/Spoons": false,
+			"Pasqually/Pasqually/Skeleton3D/Drumsticks": true,
+			"Pasqually/Pasqually/Skeleton3D/Accordion": false,
+		},
+	},
+}
+
+var HelenHouseHelen = {
+	"Helen Hair":
+	{
+		"Black and White":
+		{
+			"Helen/Helen/Skeleton3D/BWHair": true,
+			"Helen/Helen/Skeleton3D/YellowHair": false,
+		},
+		"Yellow":
+		{
+			"Helen/Helen/Skeleton3D/BWHair": false,
+			"Helen/Helen/Skeleton3D/YellowHair": true,
+		},
+	},
+	"Helen Dress":
+	{
+		"Tux":
+		{
+			"Helen/Helen/Skeleton3D/Collar": true,
+			"Helen/Helen/Skeleton3D/Torso": true,
+			"Helen/Helen/Skeleton3D/Skirt": true,
+			"Helen/Helen/Skeleton3D/CheerDress": false,
+		},
+		"Cheerleader":
+		{
+			"Helen/Helen/Skeleton3D/Collar": false,
+			"Helen/Helen/Skeleton3D/Torso": false,
+			"Helen/Helen/Skeleton3D/Skirt": false,
+			"Helen/Helen/Skeleton3D/CheerDress": true,
+		}
+	}
+}
--- Scripts/CosmeticAdjustment.gd
+++ Scripts/CosmeticAdjustment.gd
@@ -3,14 +3,15 @@ extends Control
 @export var vis_name: String
 @export var scene_handle: String
 @export var options: Dictionary
+@export var drop_index: int = 0
 
 func _ready() -> void:
 	$Panel/Label.text = vis_name
 	for option in options:
 		$Panel/OptionButton.add_item(option)
-	$Panel/OptionButton.select(0)
-	for adjusted in options[$Panel/OptionButton.get_item_text(0)]:
-		get_node(scene_handle+adjusted).visible = options[$Panel/OptionButton.get_item_text(0)][adjusted]
+	$Panel/OptionButton.select(drop_index)
+	for adjusted in options[$Panel/OptionButton.get_item_text(drop_index)]:
+		get_node(scene_handle+adjusted).visible = options[$Panel/OptionButton.get_item_text(drop_index)][adjusted]
 
 
 func _on_option_button_item_selected(index: int) -> void:
