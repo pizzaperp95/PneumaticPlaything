68aab91437de91c47d8fabde7387337610d3173f
Persephone Bubblegum-Holiday <null@localhost>
2025-08-06 21:41:20 -0700

    add mini unit 1-stage

--- Scripts/Configs/FreeRoamMaps.gd
+++ Scripts/Configs/FreeRoamMaps.gd
@@ -7,6 +7,14 @@ var Generic1Stage = {
 	"curtains": [ "Curtain" ]
 }
 
+var GenericMiniUnit1Stage = {
+	"name": "Generic Mini Unit 1-Stage",
+	"scene": "res://Scenes/FreeRoam/Generic/GenericMiniUnit1Stage.tscn",
+	"stage": Stages.MiniUnit1Stage,
+	"curtains": [ ]
+}
+
 var MapIndex = {
-	"Generic 1-Stage": Generic1Stage
+	"Generic 1-Stage": Generic1Stage,
+	"Generic Mini Unit 1-Stage": GenericMiniUnit1Stage,
 }
